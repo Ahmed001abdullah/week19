430424875946bdd6a84be772f0d216733a5e7403
Ahmed Abdullah <ahmedoffical001@gmail.com>
2023-06-01 16:05:56 +0400

    update

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'checkout.html',
+    'product.js',
+    'petstore.webmanifest',
+    'images/gymmer.png',
+    'images/icon-store-512.png'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+            caches.open(cacheName).then((cache) => {
+                console.log('[Service Worker] Caching all the files');
+                return cache.addAll(cacheFiles);
+            })
+        );
+});
+
+self.addEventListener('fetch', function (e) {   
+     e.respondWith(
+        // check if the cache has the file 
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: '              
+                + e.request.url);
+            // 'r' is the matching file if it exists in the cache
+            return r         
+        })    
+    );
+});
